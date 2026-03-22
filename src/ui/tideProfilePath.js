ea31f353159e3242786cdc36ac4a0bea809712cb
peterhoward42 <peterhoward42@gmail.com>
2026-03-22 11:42:29 +0000

    Rip out stuff manually

--- src/ui/tideProfilePath.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-/**
- * @param {import('../application/tideExtremes.js').TideExtreme[]} _extremes
- * @returns {{ d: string }}
- */
-export function tideProfilePath(_extremes) {
-  return { d: '' }
-}
