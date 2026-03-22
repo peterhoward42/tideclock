6c17fe9a0d412be82c9d3ce9520aab979cdc4004
peterhoward42 <peterhoward42@gmail.com>
2026-03-22 11:49:54 +0000

    More rip out stuff manually

--- src/application/appClock.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import { readable } from 'svelte/store'
-
-/** Milliseconds since Unix epoch, ticking once per second. */
-export const nowMs = readable(Date.now(), (set) => {
-  const id = setInterval(() => set(Date.now()), 1000)
-  return () => clearInterval(id)
-})
--- src/application/tideExtremes.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-
-/**
- * @typedef {{ time: Date, heightM: number, kind: 'high' | 'low' }} TideExtreme
- */
-
-export {}
